297e6b6f8844a51ad44a6514edce99ac3b5f5170
Matthew <cline.48@wright.edu>
2017-09-20 14:21:35 -0400

    Finished up with linear and quadratic. Still working out the bugs with the multi-variable regression model. Also need to clean up and collect graphics for the report.

--- main.py
+++ main.py
@@ -9,6 +9,7 @@
 
 import pandas as pd
 import math
+import matplotlib.pyplot as plt
 
 class Student(object):
     """A student from the flu survey data set with the following properties:
@@ -24,16 +25,217 @@ class Student(object):
         self.risk = risk
         self.respEtiq = respEtiq
 
-def costFunction(theta, x, y):
-    #enter code here
-    pass
+def costFunctionLinear(theta, trainingData):
+    ''' Compute the cost function for the linear regression '''
 
-def gradientDecent(theta0, alpha, x, y):
-    #enter code here
-    pass
+    # number of values used in the training
+    m = len(trainingData)
 
-def mean(values):
-    return sum(values) / float(len(values))
+    #list to store the predictions
+    predictions = []
+
+    # running sum of the squared error values
+    errorSum = 0
+
+    # create predictions using the current theta and calculate error
+    for student in trainingData:
+        prediction = theta[0] + theta[1] * student.knowlTrans
+        squaredError = (prediction - student.risk)**2
+        predictions.append(prediction)
+        errorSum += squaredError
+
+    # calculate the mean squared error
+    return errorSum  / (2*m)
+
+def costFunctionQuadratic(theta, trainingData):
+    ''' Compute the cost function for the linear regression '''
+
+    # number of values used in the training
+    m = len(trainingData)
+
+    #list to store the predictions
+    predictions = []
+
+    # running sum of the squared error values
+    errorSum = 0
+
+    # create predictions using the current theta and calculate error
+    for student in trainingData:
+        prediction = theta[0] + theta[1] * student.knowlTrans + theta[2] * (student.knowlTrans ** 2)
+        squaredError = (prediction - student.risk)**2
+        predictions.append(prediction)
+        errorSum += squaredError
+
+    # calculate the mean squared error
+    return errorSum  / (2*m)
+
+def costFunctionMultiVar(theta, trainingData):
+    ''' Compute the cost function for the linear regression with 2 features'''
+
+    # number of values used in the training
+    m = len(trainingData)
+
+    #list to store the predictions
+    predictions = []
+
+    #running sum of the squared error values
+    errorSum = 0
+
+    #create predictions using the current theta and calculate the error
+    for student in trainingData:
+        prediction = theta[0] + theta[1] * student.knowlTrans + theta[2] * student.respEtiq
+        squaredError = (prediction - student.risk) ** 2
+        predictions.append(prediction)
+        errorSum += squaredError
+
+    # calculate the mean squared error
+    return errorSum / (2*m)
+
+
+def gradientDescentLinear(theta0, alpha, trainingData, maxIterations):
+    ''' Perform gradient descent to minimize the cost function value to an acceptable point or stops at maxIterations '''
+    costHistory = []
+    thetaHistory = []
+    theta = theta0
+    iterations = []
+    previousCost = 0
+
+    for i in range(maxIterations):
+        predictions = []
+        errorSum0 = 0
+        errorSum1 = 0
+
+        for student in trainingData:
+            prediction = theta[0] + theta[1] * student.knowlTrans
+            error0 = prediction - student.risk
+            error1 = (prediction - student.risk) * student.knowlTrans
+            predictions.append(prediction)
+            errorSum0 += error0
+            errorSum1 += error1
+
+        thetaHistory.append(theta)
+        theta[0] = theta[0] - alpha * (1.0 / len(trainingData)) * errorSum0
+        theta[1] = theta[1] - alpha * (1.0 / len(trainingData)) * errorSum1
+
+        currentCost = costFunctionLinear(theta, trainingData)
+
+        iterations.append(i)
+
+        if abs(currentCost - previousCost) < 0.00000001:
+            costHistory.append(currentCost)
+            break
+
+        previousCost = currentCost
+        costHistory.append(currentCost)
+
+    plt.plot(iterations, costHistory, 'o')
+    plt.ylabel('Cost')
+    plt.xlabel('Iterations')
+    plt.show()
+
+    return theta, len(iterations)
+
+def gradientDescentQuadratic(theta0, alpha, trainingData, maxIterations):
+    ''' Perform gradient descent to minimize the cost function value to an acceptable point or stops at maxIterations '''
+    costHistory = []
+    thetaHistory = []
+    theta = theta0
+    iterations = []
+    previousCost = 0
+
+    for i in range(maxIterations):
+        predictions = []
+        errorSum0 = 0
+        errorSum1 = 0
+        errorSum2 = 0
+
+        for student in trainingData:
+            prediction = theta[0] + theta[1] * student.knowlTrans + theta[2] * (student.knowlTrans ** 2)
+            error0 = prediction - student.risk
+            error1 = (prediction - student.risk) * student.knowlTrans
+            error2 = (prediction - student.risk) * (student.knowlTrans ** 2)
+            predictions.append(prediction)
+            errorSum0 += error0
+            errorSum1 += error1
+            errorSum2 += error2
+
+        thetaHistory.append(theta)
+        theta[0] = theta[0] - alpha * (1.0 / len(trainingData)) * errorSum0
+        theta[1] = theta[1] - alpha * (1.0 / len(trainingData)) * errorSum1
+        theta[2] = theta[2] - alpha * (1.0 / len(trainingData)) * errorSum2
+
+        currentCost = costFunctionQuadratic(theta, trainingData)
+
+        iterations.append(i)
+
+        if abs(currentCost - previousCost) < 0.00000001:
+            costHistory.append(currentCost)
+            break
+
+        previousCost = currentCost
+        costHistory.append(currentCost)
+
+    plt.plot(iterations, costHistory, 'o')
+    plt.ylabel('Cost')
+    plt.xlabel('Iterations')
+    plt.show()
+
+    return theta, len(iterations)
+
+
+def gradientDescentMultiVar(theta0, alpha, trainingData, maxIterations):
+    ''' Perform gradient descent to minimize the cost function associated with a multi-variable regression model. '''
+
+    costHistory = []
+    thetaHistory = []
+    theta = theta0
+    iterations = []
+    previousCost = 0
+
+    for i in range(maxIterations):
+        predictions = []
+        errorSum0 = 0
+        errorSum1 = 0
+        errorSum2 = 0
+
+        for student in trainingData:
+            prediction = theta[0] + theta[1] * student.knowlTrans + theta[2] * student.respEtiq
+            error0 = prediction - student.risk
+            error1 = (prediction - student.risk) * student.knowlTrans
+            error2 = (prediction - student.risk) * student.respEtiq
+            predictions.append(prediction)
+            errorSum0 += error0
+            errorSum1 += error1
+            errorSum2 += error2
+
+        thetaHistory.append(theta)
+        theta[0] = theta[0] - alpha * (1.0 / len(trainingData)) * errorSum0
+        theta[1] = theta[1] - alpha * (1.0 / len(trainingData)) * errorSum1
+        theta[2] = theta[2] - alpha * (1.0 / len(trainingData)) * errorSum2
+
+        currentCost = costFunctionLinear(theta, trainingData)
+
+        iterations.append(i)
+
+        if abs(currentCost - previousCost) < 0.00000001:
+            costHistory.append(currentCost)
+            break
+
+        previousCost = currentCost
+        costHistory.append(currentCost)
+
+    plt.plot(iterations, costHistory, 'o')
+    plt.ylabel('Cost')
+    plt.xlabel('Iterations')
+    plt.show()
+
+    return theta, len(iterations)
+
+
+####### GLOBAL VARIABLES #######
+alpha = 0.4
+thetaInit = [2,2]
+trainingPercentage = 0.2
 
 
 ####### IMPORT DATA FROM EXCEL INTO PANDAS DATAFRAME #######
@@ -56,17 +258,64 @@ for student in data['Student']:
         continue
     cleanData.append(Student(knowlTrans, risk, respEtiq))
 
+####### PARTITION THE DATA INTO TRAINING AND TEST #######
+trainingData = []
+testData = []
+trainingSamples = len(cleanData) * trainingPercentage
 
-print(len(cleanData))
+counter = 0
+for student in cleanData:
+    if counter < trainingSamples:
+        trainingData.append(student)
+    else:
+        testData.append(student)
+    counter+=1
+
+print("####### PREPARING DATA #######")
+print('Training Samples: ', len(trainingData))
+print('Test Samples: ', len(testData))
+print("\n\n\n")
 
 
 ####### DUMMY DATASET TO EXPERIMENT WITH #######
-dummyData = [[1,1], [2,3], [3,2], [4,3], [5,5]]
-dummyX = [row[0] for row in dummyData]
-dummyY = [row[1] for row in dummyData]
+dummyStudents = []
+for i in range(5):
+    dummyStudents.append(Student(i,i,i+1))
 
-#print(mean(dummyX))
-#print(mean(dummyY))
+'''
+thetaFinal = gradientDescentLinear(thetaInit, alpha, dummyStudents, 10000)
+print("Theta Final: ", thetaFinal)
+'''
+
+####### LINEAR REGRESSION WITH ONE VARIABLE #######
+print("######## TESTING LINEAR MODEL #######")
+thetaFinal, iterations = gradientDescentLinear(thetaInit, alpha, trainingData, 100000)
+print('Iterations Needed for Convergence: ', iterations)
+print('Theta Final:', thetaFinal)
+predictionError = costFunctionLinear(thetaFinal, testData)
+print('Prediciton Error:', predictionError)
+print("\n\n\n")
+
+####### QUADRATIC REGRESSION WITH ONE VARIABLE #######
+print("####### TESTING QUADRATIC MODEL #######")
+theta = [0,0,0]
+thetaFinal, iterations = gradientDescentQuadratic(theta, alpha, trainingData, 100000)
+print('Iterations Needed for Convergence: ', iterations)
+print('Theta Final: ', thetaFinal)
+predictionError = costFunctionQuadratic(thetaFinal, testData)
+print('Prediction Error: ', predictionError)
+print("\n\n\n")
+
+####### MULTI-VARIABLE LINEAR REGRESSION #######
+print("####### TESTING MULTI_VARIABLE LINEAR REGRESSION #######")
+theta = [0,0,0]
+thetaFinal, iterations = gradientDescentMultiVar(theta, alpha, dummyStudents, 100000)
+print('Iterations Needed for Convergence: ', iterations)
+print('Theta Final: ', thetaFinal)
+predictionError = costFunctionMultiVar(thetaFinal, testData)
+print('Prediction Error: ', predictionError)
+print("\n\n\n")
 
 
-#print("Average Vaccine: ", mean(data['Vaccin']))
+######## VISUALIZATIONS ########
+
